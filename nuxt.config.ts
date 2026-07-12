f9cb0eec31ddbfe613b6e4068406e44e873ef8dc
Joshua Vicente <zho.xhie0630@gmail.com>
2026-07-12 19:54:17 +0800

    fixed resume route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,7 @@ export default defineNuxtConfig({
     }
   },
   app: {
-    baseURL: '/portfolio/',
+    baseURL: process.env.NUXT_APP_BASE_URL || '/',
     pageTransition: { name: 'system-fade', mode: 'out-in' },
     head: {
       title: 'Joshua Vicente | Full Stack Developer',
